319d14367fd09e501757ea79ee59fe2dffa86d39
Brandon Chaves <brandonjcs2011@gmail.com>
2022-12-21 15:27:43 -0600

    first commit

---
+++ About me/index.js
@@ -0,0 +1,6 @@
+// 1-Insert a script tag at the bottom.
+// 2-Change the font-family to "Arial, sans-serif" in the body "style". Replace
+// your own information in each of the spans (nickname, favorites, hometown)
+// 3-Iterate through the li, changing the class to "listitem". Set a rule for 
+// "listitem" to make the color red with a style tag.
+// 4-Make a new image element and add the "src" attribute to a photo of yourself. Insert the element into the page.
